131e8fb8ae7a1976100e2eacc12606140cda6dd2
Copper Phosphate <cu3po42@gmail.com>
2014-06-17 18:55:32 +0200

    Added Pokémon model.

---
+++ api/controllers/PokemonController.js
@@ -0,0 +1,11 @@
+/**
+ * PokemonController
+ *
+ * @description :: Server-side logic for managing pokemons
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ api/models/Pokemon.js
@@ -0,0 +1,190 @@
+/**
+* Pokemon.js
+*
+* @description :: Represents a Pokémon stored in the Database.
+* @docs        :: http://sailsjs.org/#!documentation/models
+*/
+
+
+module.exports = {
+    attributes: {
+        num: {
+            type: "INTEGER",
+            min: "1",
+            max: "718"
+        },
+        move1: {
+            type: "INTEGER",
+            required: true
+        },
+        move2: {
+            type: "INTEGER",
+            required: true
+        },
+        move3: {
+            type: "INTEGER",
+            required: true
+        },
+        move4: {
+            type: "INTEGER",
+            required: true
+        },
+        eggMove1: {
+            type: "INTEGER"
+        },
+        eggMove2: {
+            type: "INTEGER"
+        },
+        eggMove3: {
+            type: "INTEGER"
+        },
+        eggMove4: {
+            type: "INTEGER"
+        },
+        minIVHP: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVHP: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        minIVAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        minIVDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        minIVSpAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVSpAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        minIVSpDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVSpDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        minIVSpd: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        maxIVSpd: {
+            type: "INTEGER",
+            min: "0",
+            max: "31",
+            required: true
+        },
+        evHP: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        evAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        evDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        evSpAtk: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        evSpDef: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        evSpd: {
+            type: "INTEGER",
+            min: "0",
+            max: "255"
+        },
+        ot: {
+            type: "STRING",
+            required: true
+        },
+        id: {
+            type: "INTEGER",
+            max: "65536",
+            required: true
+        },
+        sid: {
+            type: "INTEGER",
+            max: "65536"
+        },
+        owner: {
+            type: "INTEGER"
+        },
+        ability: {
+            type: "INTEGER",
+            required: true
+        },
+        gender: {
+            type: "STRING",
+            required: true
+        },
+        level: {
+            type: "INTEGER",
+            min: "1",
+            max: "100"
+        },
+        shiny: {
+            type: "BOOL",
+            required: true
+        },
+        language: {
+            type: "STRING",
+            required: true
+        },
+        nickname: {
+            type: "STRING"
+        },
+        nicknameable: {
+            type: "BOOL"
+        }
+    }
+};
